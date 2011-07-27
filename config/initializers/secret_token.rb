bcecf6befc1be0d4f4c102a4847c9ea3fc97a39d
Mike Ruby <emikebr@gmail.com>
2011-07-27 10:20:44 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skepseis::Application.config.secret_token = '06330769b5b16ab51a82ebf58aa349738b338353132efedf24c485ebf6b92b370482e058877613542230a41eae13b89acfef17cb4e3a3224e089c88adeb5dad0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Skepseis::Application.config.session_store :cookie_store, :key => '_Skepseis_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Skepseis::Application.config.session_store :active_record_store
